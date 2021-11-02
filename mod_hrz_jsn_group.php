6c38e5754a61cfdbbb84252f7e6ec2b13ed98d50
alve89 <devel@stefan-herzog.com>
2021-11-02 15:25:20 +0100

    Adding social icons, fixing issues

--- mod_hrz_jsn_group.php
+++ mod_hrz_jsn_group.php
@@ -1,22 +1,53 @@
-	<?php
+<?php
 
-	// No direct access
-	defined('_JEXEC') or die;
+// No direct access
+defined('_JEXEC') or die;
 
-	// Include the syndicate functions only once
-	require_once dirname(__FILE__) . '/helper.php';
-	require_once(JPATH_SITE.'/components/com_jsn/helpers/helper.php');
+// Include the syndicate functions only once
+require_once dirname(__FILE__) . '/helper.php';
+require_once(JPATH_SITE.'/components/com_jsn/helpers/helper.php');
 
 
-	$usersID = JAccess::getUsersByGroup($params->get('groupid'));
-	$users = array();
-	foreach($usersID as $cUserID)
-	{
-		$users[] = JFactory::getUser($cUserID);
+$usersID = JAccess::getUsersByGroup($params->get('groupid'));
+$users = array();
+foreach($usersID as $cUserID) {
+	$users[] = JFactory::getUser($cUserID);
+}
+
+$i = 0;
+foreach($users as $user) {
+	$tempUser = new JsnUser($user->id);
+
+	if(!empty($tempUser->getValue('gruppenfunktion'))) {
+		// Gruppenfunktion IST definiert
+		$tempUser->gruppenfunktion = $tempUser->getValue('gruppenfunktion');
+	}
+	else {
+		// Gruppenfunktion ist NICHT definiert
+		if(!empty($tempUser->getValue('position')) && !empty($tempUser->getValue('bereich'))) {
+			$tempUser->gruppenfunktion = $tempUser->getValue('position') . ' ' . $tempUser->getValue('bereich') . ' ' . $tempUser->getValue('abteilung');
+		}
+
 	}
 
+	if(empty($tempUser->getValue('kontaktseite'))) {
+			$tempUser->kontaktadresse = $params->get('defaultContactAddress');
+	}
+	else {
+		$tempUser->kontaktadresse = $tempUser->getValue('kontaktseite');
+	}
 
-	// Render output
-	//require JModuleHelper::getLayoutPath('mod_hrz_jsn_group');
-	$layout = $params->get('layout');
-	require(JModuleHelper::getLayoutPath('mod_hrz_jsn_group', $layout ));
+	if(is_numeric($tempUser->getValue('facebook'))) {
+		$tempUser->facebook = 'profile.php?id=' . $tempUser->getValue('facebook');
+	}
+
+	// Save user object back to array
+	$users[$i] = $tempUser;
+	$i++;
+}
+
+
+// Render output
+//require JModuleHelper::getLayoutPath('mod_hrz_jsn_group');
+$layout = $params->get('layout');
+require(JModuleHelper::getLayoutPath('mod_hrz_jsn_group', $layout ));
--- tmpl/default.php
+++ tmpl/default.php
@@ -7,98 +7,51 @@ $document = Factory::getDocument();
 $document->addStyleSheet(JURI::base().'modules/'.$module->module . '/css/style.css');
 
 foreach($users as $user) {
-	$user=new JsnUser($user->id);
 
-	if(!empty($user->getValue('gruppenfunktion'))) {
-		// Gruppenfunktion IST definiert
-		$user->funktion = $user->getValue('gruppenfunktion');
-	}
-	else {
-		// Gruppenfunktion ist NICHT definiert
-		if(!empty($user->getValue('position')) && !empty($user->getValue('bereich')))  {
-			$user->funktion = $user->getValue('position') . ' ' . $user->getValue('bereich') . ' ' . $user->getValue('abteilung');
-		}
-
-	}
-
-	if(!empty($user->getValue('kontaktseite'))) {
-		// Kontaktseite IST definiert
-		if(!empty($user->getValue('convert_forms_contact_id')) && !empty($params->get('parameterName'))) {
-			$user->kontaktadresse = $user->getValue('kontaktseite') . '?' . $params->get('parameterName') . '=' . $user->getValue('convert_forms_contact_id');
-		}
-		else {
-			$user->kontaktadresse = $user->getValue('kontaktseite');
-		}
-	}
-	else {
-
-		if(!empty($user->getValue('convert_forms_contact_id')) && !empty($params->get('parameterName'))) {
-			$user->kontaktadresse = $params->get('defaultContactAddress') . '?' . $params->get('parameterName') . '=' . $user->getValue('convert_forms_contact_id');
-		}
-		else {
-			$user->kontaktadresse = $params->get('defaultContactAddress');
-		}
-	}
-
-
-!empty(parse_url($user->kontaktadresse)['query']) ? $user->kontaktadresse .= '&' : $user->kontaktadresse .= '?';
-
-$user->kontaktadresse .= 'uid='.$user->getValue('id');
-
-
+	$cssWrapperWidth = 1;
 
 ?>
 <!--<div class="jsn_group_single_profile_wrapper" style="width: <?=100/$params->get('maxElementsInRow');?>%">-->
 <div class="jsn_group_single_profile_wrapper jsn_group_col_<?=count($users)>$params->get('maxElementsInRow') ? $params->get('maxElementsInRow') : count($users);?>">
 	<div class="jsn_group_profile_wrapper" >
-		<div class="jsn_group_profile_avatar jsn_group_profile">
+		<div class="jsn_group_profile_avatar">
 			<img src="/<?=$user->getValue('avatar');?>" />
 		</div>
 		<div class="jsn_group_profile_overlay" style="background-color: <?=$params->get('backgroundcolor');?>">
-			<div class="jsn_group_profile_name jsn_group_profile" style="color: <?=$params->get('fontcolor');?>">
+			<div class="jsn_group_profile_name" style="color: <?=$params->get('fontcolor');?>">
 				<span><?=$user->getValue('firstname') . ' ' . $user->getValue('lastname');?></span>
 			</div>
 			<div class="jsn_group_profile_line" style="background-color: <?=$params->get('fontcolor');?>"></div>
-			<div class="jsn_group_profile_bereich jsn_group_profile">
-				<?=$user->funktion;?>
+			<div class="jsn_group_profile_bereich ">
+				<?=$user->gruppenfunktion;?>
 			</div>
-			<div class="jsn_group_profile_contact jsn_group_profile">
-				<div class="jsn_group_profile_contact_icon">
-					<?php
-					if($params->get('show_social')) {
-						if(!empty($user->getValue('instagram'))) { ?>
-							<div class="jsn_group_profile_layout1_social jsn_group_profile_layout1_social_instagram" style="color: <?=$params->get('fontcolor');?>; border-color: <?=$params->get('fontcolor');?>">
-								<a href="https://instagram.com/<?=$user->getValue('instagram');?>" target="_blank">
-									<i class="fa fa-instagram"></i>
-								</a>
+				<div class="jsn_group_profile_contact_icons" style="color: <?=$params->get('fontcolor');?>" >
+					<center>
+						<?php
+						if(!empty($user->getValue('facebook'))): ?>
+						<a href="https://facebook.com/<?=$user->getValue('facebook');?>" target="_blank">
+							<div class="jsn_group_profile_contact_icon">
+
+									<i class="fa fa-facebook"></i>
 							</div>
-						<?php }
-						if(!empty($user->getValue('facebook'))) { ?>
-						<div class="jsn_group_profile_layout1_social jsn_group_profile_layout1_social_facebook" style="color: <?=$params->get('fontcolor');?>; border-color: <?=$params->get('fontcolor');?>">
-							<a href="https://facebook.com/<?=$user->getValue('facebook');?>" target="_blank">
-								<i class="fa fa-facebook"></i>
-							</a>
-						</div>
-					<?php } }
-					if($params->get('show_mail')) {?>
-						<div class="jsn_group_profile_contact_icon_mail_desktop">
-							<a href="<?=$user->kontaktadresse;?>" target="_blank" style="color: <?=$params->get('fontcolor');?>; border-color: <?=$params->get('fontcolor');?>">
-								<i class="fa fa-envelope"></i>
-							</a>
-						</div>
-					<div class="jsn_group_profile_contact_icon_mail_mobile">
-						<a href="<?=$user->kontaktadresse;?>" target="_blank" style="color: <?=$params->get('fontcolor');?>; border-color: <?=$params->get('fontcolor');?>">
-							<i class="fa fa-envelope"></i>
 						</a>
-					</div>
-					<?php }
-						if($params->get('show_phone') && !empty($user->getValue('telefonnummer'))) {?>
-							<a href="tel:<?=$user->getValue('telefonnummer');?>" style="color: <?=$params->get('fontcolor');?>; border-color: <?=$params->get('fontcolor');?>">
-								<i class="fa fa-phone"></i>
-							</a>
-					<?php } ?>
+						<?php endif;
+						if(!empty($user->getValue('instagram'))): ?>
+							<div class="jsn_group_profile_contact_icon"><a href="https://instagram.com/<?=$user->getValue('instagram');?>" target="_blank"><i class="fa fa-instagram"></a></i></div>
+						<?php endif;
+						if($params->get('show_mail')): ?>
+						<div class="jsn_group_profile_contact_icon">
+							<form name="contactLink" action="<?=$params->get('defaultContactAddress');?>" method="post" target="_blank">
+						    <input type="hidden" name="<?=$params->get('parameterName');?>" value="<?=$user->getValue('convert_forms_contact_id');?>" />
+						    <input type="hidden" name="uid" value="<?=$user->getValue('id');?>" />
+						    <button type="submit" >
+						      <i class="fa fa-envelope"></i>
+						    </button>
+						  </form>
+						</div>
+						<?php endif;?>
+					</center>
 				</div>
-			</div>
 		</div>
 	</div>
 </div>
